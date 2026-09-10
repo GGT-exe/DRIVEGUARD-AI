c3b8a118d154f07ffbd8613571709ea6cb79d522
JULI <ejpachecoa@ufpso.edu.co>
2026-09-09 22:13:06 -0500

    se  hizo  una  version con mas  datos  del simulador  y  los datos  de esta  son mas  asertados

--- simulator/vehicleDataSimulator.js
+++ simulator/vehicleDataSimulator.js
@@ -1,12 +1,23 @@
+let velocidadActual = 60;
+
 function generarDatoVehiculo() {
-    const velocidad = Math.random() * 100;
-    const aceleracion = (Math.random() * 6) - 3;
+    const aceleracion = (Math.random() * 2) - 1;
+
+    velocidadActual += aceleracion;
+
+    if (velocidadActual < 0) {
+        velocidadActual = 0;
+    }
 
     return {
-        velocidad: velocidad,
-        aceleracion: aceleracion,
+        velocidad: Number(velocidadActual.toFixed(2)),
+        aceleracion: Number(aceleracion.toFixed(2)),
         timestamp: Date.now()
     };
 }
 
+console.log(generarDatoVehiculo());
+console.log(generarDatoVehiculo());
+console.log(generarDatoVehiculo());
+console.log(generarDatoVehiculo());
 console.log(generarDatoVehiculo());
